7da9a3c15841014519f6686916a5ae359bebb9d4
Asif Rahman <asif0531@live.com>
2018-01-22 23:56:39 +0600

    dummy ts added to change language detection

---
+++ app/Component/dummy.component.ts
@@ -0,0 +1,65 @@
+import { Component } from "angular2/core";
+
+
+@Component({
+    selector : 'dummy', 
+    template : ` 
+                It's a dummy Compoent, not for any uses
+                `, 
+    styles : [`
+
+    `]
+})
+export class dummyDataComponent { 
+    title : string ="Dummy title"; 
+    constructor() { 
+        console.log("Dummy Component");
+    }
+
+    dummyFunction() { 
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+    }
+    dummyFunction2() { 
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+    }
+
+    dummyFunction3() { 
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+    }
+
+    dummyFunction4() { 
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+    }
+
+    dummyFunction5() { 
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+    }
+
+    dummyFunction6() { 
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+    }
+    dummyFunction7() { 
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+        console.log("Dummy Component");
+    }
+}
